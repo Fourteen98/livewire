c1b4538e675bfa5aef0ea8243530dc9f7ffa45c8
Fourteen98 <deen.elias@gtnllc.com>
2024-06-27 21:16:15 +0000

    binding properties

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -11,4 +11,10 @@
             </div>
         @endforeach
     </div>
+
+    <form>
+        <label for="title">Title:</label>
+
+        <input type="text" id="title" wire:model.live="title">
+    </form>
 </div>
